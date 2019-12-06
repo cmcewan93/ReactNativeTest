8608b64fe8e9d9f436f845c60dbe268ca56cceaf
Colin McEwan <colin-mcewan@hotmail.com>
2019-12-06 05:57:27 -0500

    styling list boxing dynamically

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredName, setEnteredName] = useState("");
+  const [listOfNames, setListNames] = useState([]);
+
+  const nameInputHandler = enteredName => {
+    setEnteredName(enteredName);
+  };
+
+  const addNameHandler = () => {
+    console.log(enteredName);
+    console.log(listOfNames, "LIST");
+    setListNames(currentNames => [...currentNames, enteredName]);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter a name " style={styles.input} />
-        <Button title="Add"></Button>
+        <TextInput
+          placeholder="Enter a name "
+          style={styles.input}
+          onChangeText={nameInputHandler}
+          value={enteredName}
+        />
+        <Button title="Add" onPress={addNameHandler}></Button>
+      </View>
+      <View>
+        {listOfNames.map(name => (
+          <View style={styles.nameList}>
+            <Text key={name}>{name}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -29,5 +54,13 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     width: "80%"
+  },
+  //List of Names
+  nameList: {
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1
   }
 });
